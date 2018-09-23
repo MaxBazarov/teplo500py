d953f2ea148cf654bdcd133aba23a1d870372e8b
Max Bazarov <baza@odin.com>
2018-09-23 23:36:38 +0300

    Migrating to Python

--- server/run.sh
+++ server/run.sh
@@ -2,6 +2,6 @@
 COUNTER=1
 while [  $COUNTER -gt 0 ]; do
 	echo [RUNNING] The counter is $COUNTER 
-	php teplo500.php --mode real --debug
+	python teplo500.py --mode real --debug
  	let COUNTER=COUNTER+1 
 done
---
+++ server/update.py
@@ -0,0 +1,12 @@
+import CmdApp
+
+app = CmdApp.CmdApp()
+if( !app.init()):
+	log_error('Can not initiate app')
+	exit(-1)
+
+if( !app.parse_cmd_line() ):
+	exit -1;
+
+app.run_once()
+
