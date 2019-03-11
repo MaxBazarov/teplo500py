d2c6be7a268ec97dccb25fb6feb2fe0f3dc2d400
Max Bazarov <baza@odin.com>
2019-03-12 00:30:51 +0300

    Added lost file

---
+++ web/code/Teplo500Web/__init__.py
@@ -0,0 +1,25 @@
+import sys
+sys.path.insert(1, "/usr/local/teplo500/lib")
+sys.path.insert(2, "/usr/local/teplo500/web/code")
+
+from flask import Flask
+
+from teplo500web import web_app as application
+from teplo500web.pages import settings_page
+from teplo500web.pages import home_page
+from teplo500web.pages import account_page
+from teplo500web.pages import login_page
+from teplo500web.rest import client_rest
+
+flask_app = Flask(__name__)
+
+flask_app.before_request(web_app.InitBeforeRequest)
+
+home_page.home_page_register(flask_app)
+settings_page.settings_page_register(flask_app)
+account_page.account_page_register(flask_app)
+login_page.login_page_register(flask_app)
+client_rest.rest_register(flask_app)
+
+if __name__ == '__main__':
+    flask_app.run(debug=True)
